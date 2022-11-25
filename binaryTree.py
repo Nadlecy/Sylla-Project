6e74afd64c70e7136aca6701d1dcc098e9a62aee
Remy <bruno.cleymand@gmail.com>
2022-11-25 23:37:53 +0100

    update

---
+++ binaryTree.py
@@ -0,0 +1,28 @@
+"""
+DD/MM/YYYY
+25/11/2022
+
+queue data structure implementation (Python)
+by drumroll please,,, me! yes it's me again.
+"""
+
+class binaryTree():
+    def __init__(self,cell,left,right):
+        self.root=cell
+        self.left=left
+        self.right=right
+    
+    def rootData(self):
+        pass
+
+    def rightBranch(self):
+        pass
+
+    def leftBranch(self):
+        pass
+
+    def size(self): #(number of nodes)
+        pass
+
+    def height(self): #highest number of downward links from the initial root
+        pass
