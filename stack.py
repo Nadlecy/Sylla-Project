6e4665b25e6c8a00bd8308b68ae57f2d9343a7b2
Remy <bruno.cleymand@gmail.com>
2022-11-25 15:14:25 +0100

    update

--- stack.py
+++ stack.py
@@ -7,5 +7,28 @@ by Remy "Swirlinkap"
 """
 
 class stack():
-    def __init__():
+    def __init__(self, name, top = None):
+        self.name = name
+        self.top = top
+    
+    def depile(self):
+        a = self.top
+        self.top = self.top.next
+        return a
+    
+    def pile(self,newCell):
+        newCell.next = self.top
+        self.top=newCell
+    
+    def topName(self):
+        return self.top.name
+
+    def topData(self):
+        return self.top.data
         
+
+class stackCell():
+    def __init__(self,name,data,nextCell = None):
+        self.name = name
+        self.data = data
+        self.next = nextCell
